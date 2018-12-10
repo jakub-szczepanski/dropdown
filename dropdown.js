c7f968e482e00dd95cdd94d3cd0fe2c13c0e7dac
Jakub Szczepański <jakub.teodor.szczepanski@gmail.com>
2018-12-10 19:19:09 +0100

    css finished, added jQuery

--- dropdown.js
+++ dropdown.js
@@ -1,1 +1,16 @@
-$
+$(document).ready(function(){
+
+    $(".button").click(function(){
+        $(".dropdown a").removeClass("clicked");
+        $(".dropdown a").children("span").removeClass("clicked");
+        $(".arrow").toggleClass("open");
+        $(".dropdown").toggleClass("open");
+    });
+
+    $(".dropdown a").click(function(){
+        $(".dropdown a").removeClass("clicked");
+        $(".dropdown a").children("span").removeClass("clicked");
+        $(this).toggleClass("clicked");
+        $(this).children("span").toggleClass("clicked");
+    });
+});
